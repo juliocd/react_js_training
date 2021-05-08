bfdec2dc3e0d4c9bd89ccfe4c72841b0cbdceede
Julio Diaz <>
2021-05-08 15:51:42 -0500

    Passing values

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,22 @@
+import React, {useState} from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 
+import ExpensesFilter from './ExpensesFilter';
+
 const Expenses = (props) => {
     const expenses = props.expenses;
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const onChangeFilterHandler = changedFilter => {
+        setFilteredYear(changedFilter)
+    }
 
     return (
         <Card className="expenses">
+            <ExpensesFilter onChangeFilter={onChangeFilterHandler} selectedYear={filteredYear} />
             <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
             <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
             <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
